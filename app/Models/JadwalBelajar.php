8d97549f1070d72ba1dafccf48ab0bc51816be47
chikadida <chikadewanti45@gmail.com>
2025-04-18 23:30:03 +0700

    menyelesaikan edit dan menambahkan jadwal belajar

---
+++ app/Models/JadwalBelajar.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JadwalBelajar extends Model
+{
+    protected $table = 'jadwal_belajar';
+    protected $fillable = [
+        'id',
+        'user_id',
+        'nama_mapel',
+        'hari',
+        'jam',
+        'keterangan'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function mapel()
+    {
+        return $this->belongsTo(Mapel::class);
+    }
+}
