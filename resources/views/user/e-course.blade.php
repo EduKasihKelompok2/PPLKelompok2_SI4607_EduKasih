c1ff45edc2fc07606ef7b5fcf9e86008796a37ef
azkanurrizqi <mazkanurrizqi14@gmail.com>
2025-04-30 05:05:24 +0700

    memperbaiki struktur layout yang kurang

--- resources/views/user/e-course.blade.php
+++ resources/views/user/e-course.blade.php
@@ -1,27 +1,31 @@
-@extends('layout.app')
+@extends('layouts.app')
 @section('title', 'E-Course')
 @section('content')
-<div class="container">
+<div class="container py-4">
     <div class="row">
-        <h1>E-Course</h>
-        <p>Jelajahi berbagai e-course terbaik yang dirancang untuk membantu
-            siswa SMA memahami konsep dengan lebih mudah dan menyenangkan.
-            Dari Matematika hingga Bahasa, temukan materi yang sesuai dengan
-            kebutuhan belajar Anda dan tingkatkan prestasi akademik Anda!</p>
+        <div class="col">
+            <h1 class="fw-bold">E-Course</h1>
+            <p>Jelajahi berbagai e-course terbaik yang dirancang untuk membantu
+                siswa SMA memahami konsep dengan lebih mudah dan menyenangkan.
+                Dari Matematika hingga Bahasa, temukan materi yang sesuai dengan
+                kebutuhan belajar Anda dan tingkatkan prestasi akademik Anda!</p>
+        </div>
     </div>
-    <div>
-        <form method="GET" action="{{ route('e-course') }}">
-            <input type="search" name="query" value="{{ query }}">
-            <button>Cari</button>
-        </form>
-    </div>
+    <form method="GET" action="{{ route('e-course') }}">
+        <input type="text" name="query" value="{{ request('query') }}" placeholder="Cari materi...">
+        <button type="submit">Cari</button>
+    </form>
 
-    @foreach($courses as $c)
-    <div>
-        <img src="{{ $c->image }}">
-        <h2>{{ $c->judul }}</h2>
-        <p>{{ $c->desc }}</p>
-    </div>
-    @endforeach
+    @if($courses->count() > 0)
+        @foreach($courses as $course)
+        <div>
+            <img src="{{ $course->image }}" alt="Gambar">
+            <h2>{{ $course->title }}</h2>
+            <p>{{ $course->description }}</p>
+        </div>
+        @endforeach
+    @else
+        <p>Tidak ada kursus ditemukan.</p>
+    @endif
 </div>
 @endsection
